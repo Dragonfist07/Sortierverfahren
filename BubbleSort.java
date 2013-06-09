ed4d5a24c1a1d0be79a3cf85dd6d1d120dfad0e7
Dragonfist07 <jo.block@gmx.de>
2013-06-09 13:48:39 +0300

    Update BubbleSort.java
    BuubleSort wurde Kommentiert

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,24 +3,27 @@
 package BubbleSort;
 
 public class BubbleSort {
-
+	
+	
+	//DIE SORTIER METHODE!:
 	public void sortiereArray(int[] zahlen) {
-		boolean unsortiert = true;
-		int temp;
+		boolean unsortiert = true;				//bleibt so lange auf true solange es noch nicht sortiert ist
+		int temp;						//Temporäre Variable, die die zutauschende Zahl abspeichert 
 
-		while (unsortiert) {
-			unsortiert = false;
-			for (int i = 0; i < zahlen.length - 1; i++)
-				if (zahlen[i] > zahlen[i + 1]) {
-					temp = zahlen[i];
+		while (unsortiert) {					//solange das Array unsortiert ist wird es weiter sortiert
+			unsortiert = false;				//Das Programm geht erstmal davon aus das das Array sortiert ist
+									//und überprüft das später nocheinmal
+			for (int i = 0; i < zahlen.length - 1; i++)	//Die for Schleife geht durch das gesammte Array
+				if (zahlen[i] > zahlen[i + 1]) {	//Wenn die größere Zahl vor der kleineren steht, werden sie getauscht
+					temp = zahlen[i];		
 					zahlen[i] = zahlen[i + 1];
 					zahlen[i + 1] = temp;
-					unsortiert = true;
+					unsortiert = true;		//solange das Programm immer noch sortiert ist das Array unsortiert
 				}
 		}
 	}
 	
-	public void ausgabeArray(int[] zahlen){
+	public void ausgabeArray(int[] zahlen){				//Einfache Ausgabe des Arrays
 		for (int i = 0; i < zahlen.length; i++)
 			System.out.print(zahlen[i] + " ");
 	}
