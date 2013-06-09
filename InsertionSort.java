6d4774735f9cd54d00533f8d3cc4145db7f8d5e8
Dragonfist07 <jo.block@gmx.de>
2013-06-09 13:56:53 +0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,12 +4,18 @@ package InsertionSort;
 
 public class InsertionSort {
 	
+	
+	//DIE SORTIER METHODE!:
 	public void sortiere(int[] zahlen) {
-		for (int i = 1; i < zahlen.length; i++){
-			int wert = zahlen[i];
-			int position = i - 1;
+		for (int i = 1; i < zahlen.length; i++){			//Die For Schleife geht durch alle Zahlen durch
+			int wert = zahlen[i];					//Die aktuell ausgewählte Zahl von der For Schleife
+										//wird in die Variable wert eingespeichert
+			int position = i - 1;					//Da durch das imaginäre herrausnehmen der Zahl das Array nun um
+										//eins kürzer ist, ist die Position nun auch um eins kleiner
 
-			while (position >= 0 && zahlen[position] > wert) {
+			while (position >= 0 && zahlen[position] > wert) {	//Nun wird in dem Folgenden While-Schleifen-Block die Zahl an
+										//die passende Stelle eingefügt. Dafür geht die While Schleife
+										//durch das Array und schaut wo die Zahl rein passt
 				zahlen[position + 1] = zahlen[position];
 				position--;
 			}
